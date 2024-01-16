ab91acd9061fb9deba734735f07ad4019c96f64a
Matt Talbot <matthew.talbot@disney.com>
2024-01-15 22:27:36 -0500

    assume yes

--- scripts/flatpaks.sh
+++ scripts/flatpaks.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-yes | sudo flatpak install flathub us.zoom.Zoom
-yes | sudo flatpak install flathub com.slack.Slack
-yes | sudo flatpak install flathub com.visualstudio.code-oss
-yes | sudo flatpak install flathub com.vivaldi.Vivaldi
-yes | sudo flatpak install flathub com.discordapp.Discord
-yes | sudo flatpak install flathub org.libreoffice.LibreOffice
+sudo flatpak install flathub us.zoom.Zoom -y
+sudo flatpak install flathub com.slack.Slack -y
+sudo flatpak install flathub com.visualstudio.code-oss -y
+sudo flatpak install flathub com.vivaldi.Vivaldi -y
+sudo flatpak install flathub com.discordapp.Discord -y 
+sudo flatpak install flathub org.libreoffice.LibreOffice -y
