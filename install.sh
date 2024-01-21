ac4cb6fa3646d9a740bc9d5169ff9302a28ef81e
Matt Talbot <matttalbot306@gmail.com>
2024-01-20 19:31:57 -0500

    switch to nix pm

--- install.sh
+++ install.sh
@@ -15,17 +15,13 @@ if [[ $DISTRO == *"manjaro"* ]] || [[ $DISTRO == *"arch"* ]]; then
 
     # install with pacman
     yes | sudo pacman -Syu
-    yes | sudo pacman -S ufw gufw ufw-extras neofetch vim openconnect python-pygments nodejs npm curl
-    yes | sudo pamac install flatpak libpamac-flatpak-plugin
+    yes | sudo pacman -S ufw gufw ufw-extras neofetch vim openconnect python-pygments nodejs npm curl nix
 elif [[ $DISTRO == *"ubuntu"* ]] || [[ $DISTRO == *"debian"* ]]; then
     echo -e "${GREEN}>>>>> Found $DISTRO_UGLY using apt <<<<<${NC}"
 
     # install with apt
     sudo apt-get update
-    sudo apt install ufw gufw ufw-extras neofetch vim openconnect python-pygments nodejs npm curl
-    sudo apt install flatpak gnome-software-plugin-flatpak
-
-    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    sudo apt install ufw gufw ufw-extras neofetch vim openconnect python-pygments nodejs npm curl nix
 else
     echo -e "${RED}>>>>> Distro unknown exiting install <<<<<${NC}"
 
@@ -47,14 +43,22 @@ sudo ufw default allow outgoing
 echo -e "${RED}>>>>> Uncomplicated Firewall Status <<<<<${NC}"
 sudo ufw status
 
+# install nix packages
+nix-env -iA nixpkgs.vscode
+nix-env -iA nixpkgs.zoom-us
+nix-env -iA nixpkgs.slack
+nix-env -iA nixpkgs.vivaldi
+nix-env -iA nixpkgs.discord
+nix-env -iA nixpkgs.libreoffice
+
 # install flatpaks
-sudo flatpak install flathub us.zoom.Zoom -y
-sudo flatpak install flathub com.slack.Slack -y
-sudo flatpak install flathub com.visualstudio.code-oss -y
-sudo flatpak install flathub com.vivaldi.Vivaldi -y
-sudo flatpak install flathub com.discordapp.Discord -y
-sudo flatpak install flathub org.libreoffice.LibreOffice -y
-sudo flatpak install flathub com.google.Chrome -y
+# sudo flatpak install flathub us.zoom.Zoom -y
+# sudo flatpak install flathub com.slack.Slack -y
+# sudo flatpak install flathub com.visualstudio.code-oss -y
+# sudo flatpak install flathub com.vivaldi.Vivaldi -y
+# sudo flatpak install flathub com.discordapp.Discord -y
+# sudo flatpak install flathub org.libreoffice.LibreOffice -y
+# sudo flatpak install flathub com.google.Chrome -y
 
 # reboot
-echo -e "${GREEN}>>>>> Install finished time to reboot <<<<<${NC}"
+echo -e "${GREEN}>>>>> System Setup Finished <<<<<${NC}"
